c7a157eb0c84d91ade614d2ff1127a0ac2d7f17f
Pranjal Shah <shahp617@gmail.com>
2024-08-28 23:08:59 -0700

    fix init

--- app/__init__.py
+++ app/__init__.py
@@ -18,25 +18,9 @@ def create_app():
 
     with app.app_context():
         db.create_all()  # Create tables
-        add_initial_entries()  # Add initial entries
 
     from .routes.main_routes import register_all
 
     register_all(app)
 
     return app
-
-
-def add_initial_entries():
-    from .models.user import User
-
-    # Check if the database is empty before adding initial data
-    if not User.query.first():
-        initial_transactions = [
-            User(name="Pranjal"),
-            User(name="Medha")
-        ]
-
-        db.session.bulk_save_objects(initial_transactions)
-        db.session.commit()
-
