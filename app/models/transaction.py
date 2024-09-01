de1fca09523202938aea2cf85572b94564b8a87d
Pranjal Shah <shahp617@gmail.com>
2024-08-31 17:04:05 -0700

    add category to transaction

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -13,11 +13,13 @@ class Transaction(db.Model):
     credit = db.Column(db.Float, nullable=False, default=0.0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     bank_id = db.Column(db.Integer, db.ForeignKey('bank_csv.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id', name='fk_transaction_category_id'), nullable=True)
     status = db.Column(Enum('pending', 'processed', 'skip', name='status_enum'), nullable=False, default='pending')
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __init__(self, date, description, debit=0.0, credit=0.0, user_id=None, bank_id=None, **kwargs):
+    def __init__(self, date, description, debit=0.0, credit=0.0, user_id=None, bank_id=None, category_id=None,
+                 **kwargs):
         if not debit and not credit:
             raise ValueError("Either debit or credit must be provided.")
 
@@ -27,4 +29,5 @@ class Transaction(db.Model):
         self.credit = credit
         self.user_id = user_id
         self.bank_id = bank_id
+        self.category_id = category_id
         super().__init__(**kwargs)
